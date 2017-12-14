c3ee4ce22453d0f6a78b45c7ed3d7c4fcf6ba6d8
Allen Liu <allen.liu@intel.com>
2017-12-14 10:00:00 +0800

    add epoch to classifier.py
    Signed-off-by: Allen Liu <allen.liu@intel.com>

--- src/classifier.py
+++ src/classifier.py
@@ -78,18 +78,21 @@ def main(args):
             print('Calculating features for images')
             nrof_images = len(paths)
             nrof_batches_per_epoch = int(math.ceil(1.0*nrof_images / args.batch_size))
-            emb_array = np.zeros((nrof_images, embedding_size))
-            for i in range(nrof_batches_per_epoch):
-                start_index = i*args.batch_size
-                end_index = min((i+1)*args.batch_size, nrof_images)
-                paths_batch = paths[start_index:end_index]
-                images = facenet.load_data(paths_batch, False, False, args.image_size)
-                feed_dict = { images_placeholder:images, phase_train_placeholder:False }
-                emb_array[start_index:end_index,:] = sess.run(embeddings, feed_dict=feed_dict)
-            
-            classifier_filename_exp = os.path.expanduser(args.classifier_filename)
 
             if (args.mode=='TRAIN'):
+                emb_array = np.zeros((nrof_images*args.epoch, embedding_size))
+                for j in range(args.epoch):
+                  for i in range(nrof_batches_per_epoch):
+                    start_index = i*args.batch_size
+                    end_index = min((i+1)*args.batch_size, nrof_images)
+                    paths_batch = paths[start_index:end_index]
+                    images = facenet.load_data(paths_batch, True, True, args.image_size)
+                    feed_dict = { images_placeholder:images, phase_train_placeholder:False }
+                    print("start: %d, end: %d, emb_start: %d, emb_end: %d" % (start_index, end_index, (nrof_images*j + start_index), (nrof_images*j + end_index)))
+                    emb_array[(nrof_images*j + start_index):(nrof_images*j + end_index),:] = sess.run(embeddings, feed_dict=feed_dict)
+
+                labels = args.epoch * labels
+                classifier_filename_exp = os.path.expanduser(args.classifier_filename)
                 # Train classifier
                 print('Training classifier')
                 model = SVC(kernel='linear', probability=True)
@@ -104,6 +107,16 @@ def main(args):
                 print('Saved classifier model to file "%s"' % classifier_filename_exp)
                 
             elif (args.mode=='CLASSIFY'):
+                emb_array = np.zeros((nrof_images, embedding_size))
+                for i in range(nrof_batches_per_epoch):
+                    start_index = i*args.batch_size
+                    end_index = min((i+1)*args.batch_size, nrof_images)
+                    paths_batch = paths[start_index:end_index]
+                    images = facenet.load_data(paths_batch, False, False, args.image_size)
+                    feed_dict = { images_placeholder:images, phase_train_placeholder:False }
+                    emb_array[start_index:end_index,:] = sess.run(embeddings, feed_dict=feed_dict)
+
+                classifier_filename_exp = os.path.expanduser(args.classifier_filename)
                 # Classify images
                 print('Testing classifier')
                 with open(classifier_filename_exp, 'rb') as infile:
@@ -163,6 +176,8 @@ def parse_arguments(argv):
         help='Only include classes with at least this number of images in the dataset', default=20)
     parser.add_argument('--nrof_train_images_per_class', type=int,
         help='Use this number of images from each class for training and the rest for testing', default=10)
+    parser.add_argument('--epoch', type=int,
+        help='Use this number of epoch for training', default=5)
     
     return parser.parse_args(argv)
 
